034253731660f793273e33844cd3b0aa9238b0b6
Çağatay Sarıoğlan <sarioglancgty@outlook.com>
2022-12-21 12:23:09 +0300

    sum method in Main2 class.

---
+++ src/main/java/com/garanti/test/Main2.java
@@ -0,0 +1,11 @@
+package com.garanti.test;
+
+public class Main2 {
+    public static void main (String[] args) {
+
+    }
+
+    public static void sum() {
+        System.err.println(".");
+    }
+}
